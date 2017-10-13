9e692e409c7bd8aeb74a02084f8821afb25f7109
manarahmedshehata <manar.hassan1@vodafone.com>
2017-10-13 17:58:25 +0200

    test

---
+++ src/test/java/com/hellokoding/account/web/UserControllerTest.java
@@ -0,0 +1,14 @@
+package com.hellokoding.account.web;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class UserControllerTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertEquals(4, 4);
+	}
+
+}
