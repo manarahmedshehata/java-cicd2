bbea9195cd0401451fcb2ad9805b92e453f81780
manarahmedshehata <manarahmedshehata@gmail.com>
2017-12-11 11:06:51 +0200

    welcome page v 0.0.1

--- src/main/java/com/hellokoding/account/web/UserController.java
+++ src/main/java/com/hellokoding/account/web/UserController.java
@@ -48,14 +48,8 @@ public class UserController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(Model model, String error, String logout) { 
-    	if (error != null)
-            model.addAttribute("error", "Your username and password is invalid.");
-
-        if (logout != null)
-            model.addAttribute("message", "You have been logged out successfully.");
-
-        return "login";
-
+    	// just welcome page
+        return"welcome2";
     }
 
     @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
